0c801a2739a14f70d4a124f79f57cdc547ae4578
Olotuah <gbengaolotuah@gmail.com>
2024-03-11 23:10:05 +0100

    Deploy application to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import reportWebVitals from './reportWebVitals';
 const root = document.getElementById('root');
 const rootContainer = createRoot(root);
 rootContainer.render(
-  <Router basename="/frontend">
+  <Router basename="/project-klooft">
     <React.StrictMode>
       <App />
     </React.StrictMode>
